b403d74f3a059a5fb762f5ec6f20f7219e43fd9d
Jace <jach1@umbc.edu>
2021-04-01 14:26:26 -0400

    Pokinatural is now captures user IP address and stores the address in their session variable.

--- Pokinaturalists/pokinaturalist/views.py
+++ Pokinaturalists/pokinaturalist/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import exceptions
+
+from ipware import get_client_ip
 
 # Create your views here.
+def index(request):
+    client_ip = get_user_ip_addr(request)
+    if client_ip is None:
+        raise exceptions.FieldDoesNotExist("Client IP address is missing from request.")
+
+    request.session["IPv4_ADDR"] = client_ip
+    return HttpResponse(f"Client IP addr: {client_ip}")
+
+
+def get_user_ip_addr(request):
+    client_ip, _ = get_client_ip(request)
+    return client_ip
