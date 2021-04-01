39b0f5ff25f86746b31429fc14e13ee8f44a6784
Jace <jach1@umbc.edu>
2021-04-01 14:28:01 -0400

    Added unit test to ensure IP addresses are being properly extracted from requests

--- Pokinaturalists/pokinaturalist/tests.py
+++ Pokinaturalists/pokinaturalist/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.core import exceptions
+from django.http import HttpRequest, HttpResponse
 
-# Create your tests here.
+from . import views
+from ipware import get_client_ip
+
+class PokinaturalistAppTestCase(TestCase):
+    def test_for_valid_ip_addr(self):
+        request = HttpRequest()
+        request.META = {'HTTP_X_FORWARDED_FOR': '177.139.233.139, 198.84.193.157, 198.84.193.158'}  # From ipware library
+        views_ip_addr = views.get_user_ip_addr(request)
+        self.assertEqual('177.139.233.139', views_ip_addr)
+
+    def test_for_empty_ip_addr(self):
+        request = HttpRequest()
+        request.META = {}
+        self.assertIsNone(views.get_user_ip_addr(request))
