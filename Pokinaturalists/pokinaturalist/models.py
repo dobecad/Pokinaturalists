e188ed33fc2bd5fca6eb3cd8e7a7eb809aa14d0d
davidw4 <davidw4@umbc.edu>
2021-04-02 15:39:31 -0400

    Initial Commit

--- Pokinaturalists/pokinaturalist/models.py
+++ Pokinaturalists/pokinaturalist/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    token_id = models.CharField(max_length=50)
+    username = models.CharField(max_length=20, unique=True)
+    points = models.IntegerField()
+
+
+class Move(models.Model):
+    move_name = models.CharField(max_length=20)
+    move_strength = models.IntegerField()
+
+
+class Creature(models.Model):
+    owner_id = models.ForeignKey(User, on_delete=CASCADE)
+    nickname = models.CharField(max_length=50)
+    # link to the base observation(url?) Can derive picture from that
+    observation_base = models.CharField(max_length=200)
+    strength = models.IntegerField()
+    moveset = models.ManyToManyRel(Move)
+
+
+class Item(models.Model):
+    item_type = models.IntegerField()
+    item_strength = models.IntegerField()
--- Pokinaturalists/pokinaturalist/tests.py
+++ Pokinaturalists/pokinaturalist/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from pokinaturalist.models import User
 
 # Create your tests here.
+
+
+# Database  tests
+class DatabaseTests(TestCase):
+    def test_user_creation(self):
+        test = User(token_id = "893", username = "Ash Ketchum", points = 25)
+        test.save()
+        test2 = User(token_id = "6484", username = "Gary Oak", points = 50)
+        test2.save()
+        print(User.objects.all())
+        test.delete()
+        test2.delete()
+    def test_move_creation(self):
+        pass
+    def test_creature_creation(self):
+        pass
+    def test_item_creation(self):
+        pass
+    def test_user_creature(self):
+        pass
