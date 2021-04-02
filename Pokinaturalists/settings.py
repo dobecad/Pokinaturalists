97856cc75d2c6dc97f88d67091efa9d40eefe6cd
Jace <jach1@umbc.edu>
2021-04-02 18:01:27 -0400

    Moved login app to proper location

--- Pokinaturalists/settings.py
+++ Pokinaturalists/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'login',
+    'login.apps.LoginConfig',
 
     'allauth',
     'allauth.account',
