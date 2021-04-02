69bfb8e253be1e8eb13b11106808690924d6a27b
Jace <jach1@umbc.edu>
2021-04-02 16:16:42 -0400

    Added localhost to ALLOWED_HOSTS so user can still run the application locally

--- Pokinaturalists/settings.py
+++ Pokinaturalists/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pokinaturalists-jach1-test.herokuapp.com']
+ALLOWED_HOSTS = ['pokinaturalists-jach1-test.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
