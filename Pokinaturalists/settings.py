c7abfbf0a2a91cd8ab71d77dfc97e111e7ed407f
Jace <jach1@umbc.edu>
2021-04-02 15:58:42 -0400

    Moved Django project to root of github repo for deployment to heroku

--- Pokinaturalists/settings.py
+++ Pokinaturalists/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pokinaturalist.apps.PokinaturalistConfig',
+    'inaturalist.apps.InaturalistConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
