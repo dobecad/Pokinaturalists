f97eabe821e403c521e1e261e31434b01fe7bfd0
Kyle <kyle@trynainnatheart.fios-router.home>
2021-04-08 15:13:48 -0400

    changed structure of things

--- Pokinaturalists/settings.py
+++ Pokinaturalists/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'login.apps.LoginConfig',
 
     'allauth',
     'allauth.account',
