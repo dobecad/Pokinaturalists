b244ed47743c4b83ae8e4b7022a57ddb7fa8d792
Jace <jach1@umbc.edu>
2021-04-15 14:28:38 -0400

    Help Heroku see static files

--- Pokinaturalists/settings.py
+++ Pokinaturalists/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -125,3 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
+
+django_heroku.settings(locals())
