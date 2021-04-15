951d285d1f0fa0f7b5304abbc2bde901b67d49b7
IEatPencils <60478914+IEatPencils@users.noreply.github.com>
2021-04-15 19:01:14 -0400

    Revert "basic login page using google auth"

--- pokinaturalist/urls.py
+++ pokinaturalist/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from .views import HomePageView
 
 urlpatterns = [
-    path('home/', HomePageView.as_view(), name='home'),
-    path('', views.index, name='index'),
+    path('', views.index, name='index')
 ]
