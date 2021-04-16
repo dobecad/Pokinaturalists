8081a731d016867e4b37cfa87245a686e71e76cd
IEatPencils <60478914+IEatPencils@users.noreply.github.com>
2021-04-16 15:29:39 -0400

    Update urls.py
    removed conflict tags

--- pokinaturalist/urls.py
+++ pokinaturalist/urls.py
@@ -3,10 +3,9 @@ from . import views
 from .views import HomePageView
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('home/', HomePageView.as_view(), name='home'),
     path('', views.index, name='index'),
-=======
     path('', views.index, name='pokinaturalist')
->>>>>>> e1b5bc64838ab1a4e62d68acd0692e951e10e7ad
+
 ]
