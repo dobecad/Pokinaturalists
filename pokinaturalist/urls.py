ef918e07f3ba413f4d6d76aa66f5de7bb6ac8f1a
Jace <jach1@umbc.edu>
2021-04-16 17:24:06 -0400

    Fixed format bug

--- pokinaturalist/urls.py
+++ pokinaturalist/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', HomePageView.as_view(), name='home'),
+    # path('home/', HomePageView.as_view(), name='home'),
     path('', views.index, name='index'),
-    path('', views.index, name='pokinaturalist')
-    path('', views.index, name='index')
+    path('', views.index, name='pokinaturalist'),
 ]
