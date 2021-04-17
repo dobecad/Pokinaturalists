1d4eb56458c5c886c545b48e7295b8a9d6cebcac
Jace <jach1@umbc.edu>
2021-04-16 20:10:45 -0400

    Added final missing parts from revert

--- pokinaturalist/urls.py
+++ pokinaturalist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import HomePageView
 
 urlpatterns = [
-    # path('home/', HomePageView.as_view(), name='home'),
+    path('home/', HomePageView.as_view(), name='home'),
     path('', views.index, name='index'),
     path('', views.index, name='pokinaturalist')
 ]
