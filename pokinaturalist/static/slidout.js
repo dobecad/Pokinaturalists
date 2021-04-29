80fa0451d42e215a1377ca7f67a884227f932e42
IEatPencils <kmeyer5@umbc.edu>
2021-04-29 17:27:02 -0400

    made the slideout fade away the center icon

--- pokinaturalist/static/slidout.js
+++ pokinaturalist/static/slidout.js
@@ -3,17 +3,40 @@
 
 function openNav() {
     var trainer = document.getElementsByClassName("leaflet-marker-icon leaflet-zoom-animated leaflet-interactive")[0];
-    if(trainer)
-    {
-        console.log("found trainer");
-    }
     document.getElementById("myNav").style.width = "100%";
-    trainer.style.display = "none";
+    fade(trainer);
   }
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeNav() {
     var trainer = document.getElementsByClassName("leaflet-marker-icon leaflet-zoom-animated leaflet-interactive")[0];
     document.getElementById("myNav").style.width = "0%";
-    trainer.style.display = "block";
+    unfade(trainer);
   }
+
+
+  function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.2){
+            clearInterval(timer);
+            element.style.opacity = '0.2';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+    var op = 0.2;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
