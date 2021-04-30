4d26248783a9ac7985f42f81cba02d6dc946c402
davidw4 <davidw4@umbc.edu>
2021-04-29 22:56:26 -0400

    Added comments and fixed tests some more

--- pokinaturalist/tests.py
+++ pokinaturalist/tests.py
@@ -34,6 +34,12 @@ class PokinaturalistAppTestCase(TestCase):
         request.META = {}
         self.assertIsNone(views.get_user_ip_addr(request))
 
+from django.test import TestCase
+from django.db import IntegrityError
+from .models import User, Move, Creature, Item
+
+# Create your tests here.
+
 
 # Database  tests(for local database)
 class DatabaseTests(TestCase):
@@ -42,6 +48,89 @@ class DatabaseTests(TestCase):
         test = User(token_id="893", username="Ash Ketchum", points=25)
         test.save()
 
+    def test_user_creation(self):
+        test2 = User(token_id="6484", username="Gary Oak", points=50)
+        test2.save()
+
+    # add a duplicate user test just to be sure of unique status of username
+    def test_duplicate_username(self):
+        try:
+            test2 = User(token_id="8064", username="Ash Ketchum", points=12)
+            test2.save()
+        except IntegrityError:
+            return True
+        return False
+    def test_move_creation(self):
+        test2 = Move(move_name="Thundershock", move_strength=50)
+    # add a duplicate move test just to be sure of unique status of username
+    def test_duplicate_move(self):
+        try:
+            test2 = Move(move_name="Thundershock", move_strength=30)
+            test3 = Move(move_name="Thundershock", move_strength=50)
+            test2.save()
+            test3.save()
+        except IntegrityError:
+            return True
+        return False
+    def test_creature_creation(self):
+        test2 = User.objects.get(username="Ash Ketchum")
+        test3 = Creature(owner_id=test2, nickname="Pikachu", observation_base="Pokemon num 25", strength=1337)
+        test4 = Move(move_name="Thunderbolt", move_strength=42)
+        test3.save()
+        test4.save()
+        test3.moveset.add(test4)
+
+    def test_item_creation(self):
+        test2 = User.objects.get(username="Ash Ketchum")
+        test3 = Item(item_type=1, item_strength=50)
+        test3.owner_id = test2
+
+    #user to creatures
+    def test_multiple_creature(self):
+        # this will test interaction between user and creature by attaching multiple pokemon
+        # to the setup of THE POKEMON MASTER(Ash)
+        test2 = User.objects.get(username="Ash Ketchum")
+        test3 = Creature(owner_id=test2, nickname="Pikachu", observation_base="Pokemon num 25", strength=1337)
+        test4 = Creature(owner_id=test2, nickname="Charizard", observation_base="Blaze it", strength=420)
+
+    #creature to moves
+    def test_multiple_moves(self):
+        test2 = User.objects.get(username="Ash Ketchum")
+        test3 = Creature(owner_id=test2, nickname="Pikachu", observation_base="Pokemon num 25", strength=1337)
+        test4 = Move(move_name="Thunderbolt", move_strength=42)
+        test5 = Move(move_name="Volt Tackle", move_strength=69)
+        test3.save()
+        test4.save()
+        test5.save()
+        test3.moveset.add(test4)
+        test3.moveset.add(test5)
+
+    #user to items
+    def test_multiple_items(self):
+        # let's assume ash has 2 potions
+        test2 = User.objects.get(username="Ash Ketchum")
+        test3 = Item(owner_id=test2, item_type=1, item_strength=50)
+        test4 = Item(owner_id=test2, item_type=1, item_strength=50)
+
+#These will be simples tests for the POSTGRES database since SQLite can test rest.
+class PostgresDatabaseTests(TestCase):
+    @classmethod
+    def setUp(cls):
+        pass
+
+    def test_creation(self):
+        pass
+
+
+
+# Database  tests(for local database)
+class DatabaseTests(TestCase):
+    @classmethod
+    def setUp(cls):
+        test = User(token_id="893", username="Ash Ketchum", points=25)
+        test.save()
+
+    # Naive User creation
     def test_user_creation(self):
         test2 = User(token_id="6484", username="Gary Oak", points=50)
         test2.save()
@@ -55,9 +144,13 @@ class DatabaseTests(TestCase):
         except IntegrityError:
             return True
         return False
+
+    #Naive move creation
     def test_move_creation(self):
         test2 = Move(move_name="Thundershock", move_strength=50)
+        test2.save()
         self.assertEqual(Move.objects.get(), test2)
+
     # add a duplicate move test just to be sure of unique status of username
     def test_duplicate_move(self):
         try:
@@ -68,16 +161,19 @@ class DatabaseTests(TestCase):
         except IntegrityError:
             return True
         return False
+
+    #Naive Creature creation with a move
     def test_creature_creation(self):
         test2 = User.objects.get(username="Ash Ketchum")
         test3 = Creature(owner_id=test2, nickname="Pikachu",
-                         observation_icon="Pokemon num 25", observation_wiki="Pokemon num 25", strength=1337)
+                         observation_photo="Shocking", observation_wiki="Pokemon num 25", strength=1337)
         test4 = Move(move_name="Thunderbolt", move_strength=42)
         test3.save()
         test4.save()
         test3.moveset.add(test4)
         self.assertEqual(Creature.objects.get(), test3)
 
+    # Naive Item creation
     def test_item_creation(self):
         test2 = User.objects.get(username="Ash Ketchum")
         test3 = Item(item_type=1, item_strength=50)
@@ -85,22 +181,24 @@ class DatabaseTests(TestCase):
         test3.save()
         self.assertEqual(Item.objects.get(), test3)
 
-    #user to creatures
+    #Tests multiple creatures to a user
     def test_multiple_creature(self):
         # this will test interaction between user and creature by attaching multiple pokemon
         # to the setup of THE POKEMON MASTER(Ash)
         test2 = User.objects.get(username="Ash Ketchum")
         test3 = Creature(owner_id=test2, nickname="Pikachu",
-                         observation_icon="Defibrillator", observation_wiki="Pokemon num 25", strength=1337)
+                         observation_photo="Defibrillator", observation_wiki="Pokemon num 25", strength=1337)
         test4 = Creature(owner_id=test2, nickname="Charizard",
-                         observation_icon="Blaze it", observation_wiki="Pokemon num 6", strength=420)
+                         observation_photo="Blaze it", observation_wiki="Pokemon num 6", strength=420)
         test3.save()
         test4.save()
         self.assertEqual(Creature.objects.filter(owner_id=test2).count(), 2)
-    #creature to moves
+
+    #Tests multiple moves to a creature
     def test_multiple_moves(self):
         test2 = User.objects.get(username="Ash Ketchum")
-        test3 = Creature(owner_id=test2, nickname="Pikachu", observation_base="Pokemon num 25", strength=1337)
+        test3 = Creature(owner_id=test2, nickname="Pikachu",
+                         observation_photo="Defibrillator", observation_wiki="Pokemon num 25", strength=1337)
         test4 = Move(move_name="Thunderbolt", move_strength=42)
         test5 = Move(move_name="Volt Tackle", move_strength=69)
         test3.save()
@@ -110,7 +208,7 @@ class DatabaseTests(TestCase):
         test3.moveset.add(test5)
         self.assertEqual(Creature.objects.get(nickname="Pikachu").moveset.count(), 2)
 
-    #user to items
+    #Tests multiple items to a user
     def test_multiple_items(self):
         # let's assume ash has 2 potions
         test2 = User.objects.get(username="Ash Ketchum")
