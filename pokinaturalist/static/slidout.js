a223824244a01c69decdc22989e5a4c679efc2da
IEatPencils <kmeyer5@umbc.edu>
2021-05-03 16:14:05 -0400

    further testing

--- pokinaturalist/static/slidout.js
+++ pokinaturalist/static/slidout.js
@@ -40,3 +40,26 @@ function unfade(element) {
         op += op * 0.1;
     }, 10);
 }
+
+// this is for the text box 
+// retrieve the element
+"use strict";
+element = document.getElementById("display");
+
+// reset the transition by...
+element.addEventListener("click", function(e) {
+  e.preventDefault;
+  
+  // -> removing the class
+  element.classList.remove("typewriter");
+  
+  // -> triggering reflow /* The actual magic */
+  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+  // Oops! This won't work in strict mode. Thanks Felis Phasma!
+  // element.offsetWidth = element.offsetWidth;
+  // Do this instead:
+  void element.offsetWidth;
+  
+  // -> and re-adding the class
+  element.classList.add("typewriter");
+}, false);
